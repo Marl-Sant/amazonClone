ebde59c854b5337ee0ab54c9af928547eba4bbbb
Marl-Sant <santosfmarlon@gmail.com>
2023-09-27 14:19:18 -0400

    added more user sign up validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a name with at least 4 characters.'),
+    check('phoneNum')
+      .isMobilePhone()
+      .withMessage('Please provide a valid phone number.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+// Sign up
 router.post(
     '/',
+    validateSignup,
     async (req, res) => {
         const { email, password, name, phoneNum } = req.body;
         const hashedPassword = bcrypt.hashSync(password);
