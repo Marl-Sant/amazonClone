82f396aca11a61e7070a09d5cae90d231280d4de
Marl-Sant <santosfmarlon@gmail.com>
2023-09-27 15:36:59 -0400

    started new address mig and models on dev by accident

---
+++ backend/db/migrations/20230927193231-create-addresses.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Addresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      street: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      addressPhoneNum: {
+        type: Sequelize.STRING
+      },
+      postCode: {
+        type: Sequelize.STRING
+      },
+      defaultAddress: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Addresses');
+  }
+};
